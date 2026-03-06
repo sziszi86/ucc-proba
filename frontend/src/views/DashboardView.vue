f5efac6e6b465675977a3985976d0afbfe8fc588
salamonszilard <salamonszilard@gmail.com>
2026-03-06 21:57:56 +0100

    style: Move security banner close button to top left

--- frontend/src/views/DashboardView.vue
+++ frontend/src/views/DashboardView.vue
@@ -117,8 +117,8 @@ const recentActivity = computed(() => {
 
     <!-- Security Tip Banner -->
     <div v-if="!authStore.user?.mfa_enabled && showSecurityTip" class="animate-in mb-10 bg-gradient-to-r from-amber-50 to-orange-50 border border-amber-100 rounded-3xl p-6 flex flex-col md:flex-row items-center justify-between shadow-sm relative group">
-      <button @click="showSecurityTip = false" class="absolute top-4 right-4 p-1 text-amber-400 hover:text-amber-600 transition-colors opacity-0 group-hover:opacity-100">
-        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+      <button @click="showSecurityTip = false" class="absolute top-2 left-2 p-1.5 text-amber-400 hover:text-amber-600 transition-colors opacity-0 group-hover:opacity-100 bg-white/50 rounded-full shadow-sm">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
           <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" />
         </svg>
       </button>
